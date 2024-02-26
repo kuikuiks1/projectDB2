083aaea198f8e1fc243ed6fbf957c5ed204cb27d
TomiKaufmann <tomikaufmann56@gmail.com>
2024-02-26 18:57:09 -0300

    cambios html

--- Proyecto app web mejorado/app/categoria_usuario.py
+++ Proyecto app web mejorado/app/categoria_usuario.py
@@ -1,3 +1,20 @@
+from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
+
+
+try:
+    # Intentamos establecer la conexión con MongoDB Atlas
+    client = MongoClient('mongodb+srv://kikiazcoaga:uade123@projectbd2.jcixys6.mongodb.net/'
+                         '?retryWrites=true&w=majority&appName=ProjectBD2')
+
+    # Seleccionamos la base de datos y la colección
+    db = client['ProjectDB2']
+    collection_sessions = db['Sessions']
+
+except ConnectionFailure as e:
+    print(f"Error de conexión a MongoDB Atlas: {e}")
+    exit()
+    
 def categorizar_usuario():
     email_usuario = input("Ingrese el email del usuario para saber la categorización: ")
     sesiones = collection_sessions.find({"email": email_usuario})
@@ -6,10 +23,15 @@ def categorizar_usuario():
     for sesion in sesiones:
         num_total_actividades += 1
 
-    print("Número total de actividades:", num_total_actividades) 
     if num_total_actividades >= 10:
+        print("Número total de actividades:", num_total_actividades)
         print("Categoría TOP")
     elif num_total_actividades >= 5:
+        print("Número total de actividades:", num_total_actividades)
         print("Categoría MEDIUM")
-    else:
+    elif num_total_actividades >= 1:
+        print("Número total de actividades:", num_total_actividades)
         print("Categoría LOW")
+    else:
+        print("No se encuentra el usuario.")
+categorizar_usuario()
